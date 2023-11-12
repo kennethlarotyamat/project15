eb0e4349bcec3737860cf82099714952c2c7668b
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-12 12:21:36 -0800

    1

--- misc/reactbuttons.js
+++ misc/reactbuttons.js
@@ -1,7 +1,7 @@
-function Hello() {
+function Hello1() {
     return <h1>Here are a few other things I'd Like to tell you about</h1>;
   }
 
   const container = document.getElementById('mydiv3');
   const root = ReactDOM.createRoot(container);
-  root.render(<Hello />)
+  root.render(<Hello1 />)
