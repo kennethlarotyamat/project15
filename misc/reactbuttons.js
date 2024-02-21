0ba0a81e2299fd4fe891498c8ac35aa2bcfa1d9d
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-21 04:29:34 -0800

    1

--- misc/reactbuttons.js
+++ misc/reactbuttons.js
@@ -5,3 +5,16 @@ function Hello1() {
   const container = document.getElementById('mydiv3');
   const root = ReactDOM.createRoot(container);
   root.render(<Hello1 />)
+
+  /*
+
+
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
+
+
+
+*/
--- misc/refactoredproject12.js
+++ misc/refactoredproject12.js
@@ -8,7 +8,18 @@ $(function(){
   });  
 $(function() {
   "use strict";
+/*
 
+
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
+
+
+
+*/
   let bodyA = $(".bodya");
   let footer = $(".footer");
   let main = $(".main");
